f4200b29d487d5b6b1dce1853647bc39f7da595e
PloytawanPN <95073452+PloytawanPN@users.noreply.github.com>
2021-12-22 18:11:23 +0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,18 +1,33 @@
 import { ApolloServer, gql } from 'apollo-server';
 
+const  machines = [
+    { id : "01", brand: "Incubator_A",vertion: "v.4"},
+    { id : "02", brand: "Incubator_B",vertion: "v.6"},
+    { id : "03", brand: "Incubator_C",vertion: "v.2"},
+];
 //schema
 const typeDefs = gql`
     type Query {
-        hello: String
+        machines:[Machine]
+        machine(id: String) : Machine
+    }
+    type Machine{
+        id : String
+        brand : String
+        vertion : String
     }
 `;
 
 //resolver
 const resolvers = {
     Query:{
-        hello: (parent, args, context, info) => {
-            return "world";
-        }
+        machines: (parent,args,context ,info)=>{
+            return machines;
+        },
+        machine: (parent,args,context ,info)=>{
+            return machine.find(machine=> machine.id === args.id);
+        },
+
     }
 };
 
